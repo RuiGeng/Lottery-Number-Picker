6100add43b1939d0747bdffd9a1a7c5b61866de0
Rui Geng <gengr.r@gmail.com>
2017-10-22 22:11:43 -0400

    add a simple model

---
+++ LotteryPicker/LotteryPicker/Models/LottoMax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LotteryPicker.Models
+{
+    public class LottoMax
+    {
+        public IReadOnlyList<int> Numbers;
+
+        private const int Min = 1;
+
+        private const int Max = 50;
+
+        public LottoMax()
+        {
+            Numbers = new List<int>();
+        }
+
+        public void GenerateNumbers()
+        {
+            Numbers = Enumerable
+                .Range(Min, Max)
+                .OrderBy(g => Guid.NewGuid())
+                .Take(7)
+                .ToArray();
+        }
+
+        public void PrintNumbers()
+        {
+            foreach (var number in Numbers)
+            {
+                Console.WriteLine($@"{number}");
+            }
+        }
+    }
+}
