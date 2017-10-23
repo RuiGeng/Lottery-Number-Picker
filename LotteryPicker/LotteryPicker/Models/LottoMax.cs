bfe2c30ad97337e0b5b829826f87411f19886e72
Rui Geng <gengr.r@gmail.com>
2017-10-22 22:25:38 -0400

    implement INotifyPropertyChanged

--- LotteryPicker/LotteryPicker/Models/LottoMax.cs
+++ LotteryPicker/LotteryPicker/Models/LottoMax.cs
@@ -1,29 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace LotteryPicker.Models
 {
-    public class LottoMax
+    public class LottoMax : INotifyPropertyChanged
     {
-        public IReadOnlyList<int> Numbers;
+        public ObservableCollection<int> Numbers
+        {
+            get { return numbers; }
+            set
+            {
+                if (value == numbers)
+                {
+                    return;
+                }
+                numbers = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<int> numbers;
 
         private const int Min = 1;
 
         private const int Max = 50;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public LottoMax()
         {
-            Numbers = new List<int>();
+            Numbers = new ObservableCollection<int>();
         }
 
         public void GenerateNumbers()
         {
-            Numbers = Enumerable
+            var resultInts = Enumerable
                 .Range(Min, Max)
                 .OrderBy(g => Guid.NewGuid())
                 .Take(7)
                 .ToArray();
+
+            Numbers = new ObservableCollection<int>(resultInts);
         }
 
         public void PrintNumbers()
